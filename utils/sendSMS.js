b371aaf761079a63105a42a53a159535f4d97331
Roy <royjosephp@gmail.com>
2021-03-07 22:14:57 +0530

    Update sms code

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,20 +1,17 @@
-var http = require('http');
+const { default: axios } = require('axios');
+var http = require('axios');
 
 
 async function sendSMS(phone, OTPString) {
 
-    var options = {
-        'method': 'POST',
-        'hostname': process.env.SMS_HOSTNAME,
-        'path': `${process.env.SMS_PATH}&numbers=${phone}&message=Your haritham one time password is : ${OTPString}`,
-        'headers': {
-        },
-        'maxRedirects': 20
-      };
+  var config = {
+    method: 'post',
+    url: `${process.env.SMS_HOSTNAME}${process.env.SMS_PATH}&numbers=${phone}&message=Your haritham one time password is : ${OTPString}`,
+    headers: {}
+  };
 
-    var req = http.request(options, function (res) {});
-    
-    req.end();
+  await axios(config)
+
 }
 
-module.exports = {sendSMS};
+module.exports = { sendSMS };

60ad4608f2221d556598746c1ecc9908f0245ba7
Roy <royjosephp@gmail.com>
2021-03-07 23:27:48 +0530

    change sms code

--- middleware/error.js
+++ middleware/error.js
@@ -16,6 +16,7 @@ const errorHandler = (err, req, res, next) => {
   }
 
   console.log(error.message);
+  console.log(error);
 
   res.status(error.statusCode || 500).json({
     success: false,
--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,17 +1,37 @@
-const { default: axios } = require('axios');
-var http = require('axios');
+var http = require('http');
+var axios = require('axios');
 
 
 async function sendSMS(phone, OTPString) {
 
-  var config = {
-    method: 'post',
-    url: `${process.env.SMS_HOSTNAME}${process.env.SMS_PATH}&numbers=${phone}&message=${OTPString}`,
-    headers: {}
+  var options = {
+    'method': 'POST',
+    'hostname': process.env.SMS_HOSTNAME,
+    'path': `${process.env.SMS_PATH}&numbers=${phone}&message=Your%20haritham%20one%20time%20password%20is%20%3A%20${OTPString}`,
+    'headers': {
+    },
+    'maxRedirects': 20
   };
 
-  await axios(config)
+  var req = http.request(options, function (res) {
+    var chunks = [];
 
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+
+    res.on("error", function (error) {
+      console.error(error);
+    });
+
+  });
+
+  req.end();
 }
 
 module.exports = { sendSMS };

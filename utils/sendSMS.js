1314262c8456aff71ae43379cd31ed665b35a6b8
Roy <royjosephp@gmail.com>
2021-03-07 22:20:04 +0530

    Update sms code

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -6,7 +6,7 @@ async function sendSMS(phone, OTPString) {
 
   var config = {
     method: 'post',
-    url: `${process.env.SMS_HOSTNAME}${process.env.SMS_PATH}&numbers=${phone}&message=Your haritham one time password is : ${OTPString}`,
+    url: `${process.env.SMS_HOSTNAME}${process.env.SMS_PATH}&numbers=${phone}&message=${OTPString}`,
     headers: {}
   };
 

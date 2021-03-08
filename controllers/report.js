f9917e284759100eba8c60c046d01740450d57e3
Roy <royjosephp@gmail.com>
2021-03-08 17:15:37 +0530

    Add error msg for image

--- controllers/report.js
+++ controllers/report.js
@@ -1,6 +1,7 @@
 const Report = require("../models/Report");
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.getReports = async (req, res, next) => {
     try {
@@ -44,14 +45,14 @@ exports.getReportById = async (req, res, next) => {
 
 exports.addReport = async (req, res, next) => {
     const { location, state, district, type, description } = req.body;
+    if(!req.file){
+      throw new ErrorResponse("Image is missing");
+    }
     const { filename, mimetype } = req.file;
 
     console.log(location)
 
     try {
-      console.log(req.file.path);
-      console.log(req.get('host'));
-      console.log(req.protocol);
         const report = await Report.create({
             location : JSON.parse(location),
             state,
